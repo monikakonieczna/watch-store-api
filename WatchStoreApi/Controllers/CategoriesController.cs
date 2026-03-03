013197ac68a7a2e542d31631f15dd6c31e941356
monika_konieczna <konieczna.monika01@gmail.com>
2026-03-03 11:17:14 +0100

    Added categories controllers action methods

---
+++ WatchStoreApi/Controllers/CategoriesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WatchStoreApi.Data;
+using WatchStoreApi.Models;
+
+namespace WatchStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        private ApiDbContext dbContext;
+        public CategoriesController(ApiDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        // GET: api/<CategoriesController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+           var categories = await dbContext.Categories.ToListAsync();
+           return Ok(categories);
+        }
+
+        // POST api/<CategoriesController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Category category)
+        {
+            if (category == null)
+            {
+                return BadRequest("Category is null");
+            }
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        // PUT api/<CategoriesController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Category category)
+        {
+            var existingCategory = await dbContext.Categories.FindAsync(id);
+            if (existingCategory == null)
+            {
+                return NotFound("Category not found");
+            }
+            existingCategory.Name = category.Name;
+            await dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK, "Record updated");
+        }
+
+        // DELETE api/<CategoriesController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var existingCategory = await dbContext.Categories.FindAsync(id);
+            if (existingCategory == null)
+            {
+                return NotFound("Category not found");
+            }
+            dbContext.Categories.Remove(existingCategory);
+            await dbContext.SaveChangesAsync();
+            return Ok("Record has been deleted");
+        }
+    }
+}
--- WatchStoreApi/Models/Category.cs
+++ WatchStoreApi/Models/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace WatchStoreApi.Models
+﻿using System.Text.Json.Serialization;
+
+namespace WatchStoreApi.Models
 {
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [JsonIgnore]
         public ICollection<Product> Products { get; set; }
     }
 }
