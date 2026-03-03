7a2d5c871bd31da3790c9617dd877c3ffc168e8a
monika_konieczna <konieczna.monika01@gmail.com>
2026-03-03 10:23:57 +0100

    Implementing http status codes

--- WatchStoreApi/Controllers/ProductsController.cs
+++ WatchStoreApi/Controllers/ProductsController.cs
@@ -18,27 +18,42 @@ namespace WatchStoreApi.Controllers
         }
 
         [HttpGet]
-        public async Task<IEnumerable<Product>> Get()
+        public async Task<IActionResult> Get()
         {
-            return await dbContext.Products.ToListAsync();
+            var products = await dbContext.Products.ToListAsync();
+            if(products == null || products.Count == 0)
+            {
+                return NotFound();
+            }
+            return  Ok(products);
         }
 
         [HttpGet("{id}")]
-        public async Task<Product> Get(int id)
+        public async Task<ActionResult> Get(int id)
         {
-            return await dbContext.Products.FirstOrDefaultAsync(p=>p.Id == id);
+            var product = await dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
         }
 
         [HttpPost]
-        public async Task Post([FromBody] Product product)
+        public async Task<ActionResult> Post([FromBody] Product product)
         {
+            if(product == null)
+            {
+                return BadRequest("Product is null");
+            }
             await dbContext.Products.AddAsync(product);
             await dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status201Created);
         }
 
         // api/products/id
         [HttpPut("{id}")]
-        public async Task Put(int id, [FromBody] Product product)
+        public async Task<IActionResult> Put(int id, [FromBody] Product product)
         {
             var existingProduct = await dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
             if(existingProduct != null)
@@ -47,19 +62,23 @@ namespace WatchStoreApi.Controllers
                 existingProduct.Description = product.Description;
                 existingProduct.Price = product.Price;
                 await dbContext.SaveChangesAsync();
+                return Ok("Record updated");
             }
+            return NotFound("Record not found");
         }
 
         // api/products/id
         [HttpDelete("{id}")]
-        public async Task Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
             var existingProduct = dbContext.Products.FirstOrDefault(p => p.Id == id);
             if (existingProduct != null)
             {
                 dbContext.Products.Remove(existingProduct);
                 await dbContext.SaveChangesAsync();
+                return Ok("Record has been deleted");
             }
+            return NotFound("Record not found");
         }
     }
 }
